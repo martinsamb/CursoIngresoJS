3d282fceac89527d1837a5993c22a213525047c4
alumno <alumno>
2019-02-05 14:15:01 -0300

    Ejercicios 5,6 y 7

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,21 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var primerNumero;
+	var segundoNumero;
+	var suma;	
 
+	primerNumero = numeroUno.value;
+	segundoNumero = numeroDos.value;
+
+
+	//alert(primerNumero+segundoNumero);
+
+	primerNumero = parseInt(primerNumero);//transformo en entero
+	segundoNumero = parseInt(segundoNumero);
+
+	suma = primerNumero+segundoNumero;
+
+	alert("La suma es = " + suma);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,69 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero = numeroUno.value;
+	segundoNumero = numeroDos.value;
+
+	primerNumero = parseInt(primerNumero);
+	segundoNumero = parseInt(segundoNumero);
+
+	resultado = primerNumero+segundoNumero;
+
+	alert("La suma es = " + resultado);
 }
 
 function restar()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero = numeroUno.value;
+	segundoNumero = numeroDos.value;
+
+	primerNumero = parseInt(primerNumero);//transformo en entero
+	segundoNumero = parseInt(segundoNumero);
+
+	resultado = primerNumero-segundoNumero;
+
+	alert("La resta es = " + resultado);	
 }
 
 function multiplicar()
 { 
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero = numeroUno.value;
+	segundoNumero = numeroDos.value;
+
+	primerNumero = parseInt(primerNumero);//transformo en entero
+	segundoNumero = parseInt(segundoNumero);
+
+	resultado = primerNumero*segundoNumero;
+
+	alert("La multiplicación es = " + resultado);	
 }
 
 function dividir()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero = numeroUno.value;
+	segundoNumero = numeroDos.value;
+
+	primerNumero = parseInt(primerNumero);//transformo en entero
+	segundoNumero = parseInt(segundoNumero);
+
+	resultado = primerNumero/segundoNumero;
+
+	alert("La división es = " + resultado);	
 }
 
